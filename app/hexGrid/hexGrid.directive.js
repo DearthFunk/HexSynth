5c0d9efcaa07aa70c427247dd1012d1c9155e72e
tylorspence <tylor.spence@conceptshare.com>
2015-04-03 21:49:20 -0400

    start of hexGridService to hexGridDirective

--- app/hexGrid/hexGrid.directive.js
+++ app/hexGrid/hexGrid.directive.js
@@ -1,28 +1,47 @@
-angular.module('hexGridModule', [])
-    .service("hexCanvasService", hexCanavasService);
+angular
+	.module('hexGridModule', [])
+    .directive('hexGrid', hexGrid);
 
-	hexCanavasService.$inject = ['$window', 'themeService', 'controlsService', 'audioService', 'localStorageService'];
+	hexGrid.$inject = [];
 
-	function hexCanavasService($window, themeService, controlsService, audioService, localStorageService){
+	function hexGrid() {
+		var directive = {
+			restrict: 'EA',
+			replace: true,
+			controller: hexGridController,
+			bindToController: true
+		};
+		return directive;
+	}
 
-        var hexCanvas = this;
-        var w, h, lastHoverIndex = -1;
-        var cnv = document.querySelectorAll('.hexCanvas')[0];
-        var ctx = cnv.getContext("2d");
+	hexGridController.$inject = ['$scope', '$element', '$window', 'themeService', 'controlsService', 'audioService', 'localStorageService'];
+
+	function hexGridController($scope, $element, $window, themeService, controlsService, audioService, localStorageService){
+
+		var hexSize = angular.isObject(localStorageService.storage) ? localStorageService.storage.hexSize : 0.5;
+		var notes = ['A','A\'','B','C','C\'','D','D\'','E','F','F\'','G','G\''];
+        var w, h, hoverIndex, lastHoverIndex = -1;
+        var ctx = $element[0].getContext('2d');
 		var hexScreenPadding = 30;
-		var notes = ["A","A '","B","C","C '","D","D '","E","F","F '","G","G '"];
-        hexCanvas.hexGrid = {};
-        hexCanvas.hoverIndex = -1;
-        hexCanvas.hexSize = angular.isObject(localStorageService.storage) ? localStorageService.storage.hexSize : 0.5;
+        var hexGrid = {};
 
-        hexCanvas.windowResize = function() {
+		$scope.windowResize = windowResize;
+		$scope.recalculateAndDrawHexes = recalculateAndDrawHexes;
+		$scope.drawHex = drawHex;
+		$scope.checkHexes = checkHexes;
+
+		windowResize();
+
+		////////////////////////////////////////////////
+		
+        function windowResize() {
             w = $window.innerWidth;
             h = $window.innerHeight;
-            cnv.style.width = w +'px';
-            cnv.style.height = h + 'px';
-            angular.element(cnv).attr({width:  w, height: h	});
-            hexCanvas.recalculateAndDrawHexes(true);
-        };
+            ctx.canvas.style.width = w +'px';
+            ctx.canvas.style.height = h + 'px';
+            angular.element(ctx.canvas).attr({width:  w, height: h	});
+            $scope.recalculateAndDrawHexes(true);
+        }
 		function pointInPolygon( vertx, verty, testx, testy ) {
 			var i, j, c = false;
 			for( i = 0, j = vertx.length-1; i < vertx.length; j = i++ ) {
@@ -33,64 +52,64 @@ angular.module('hexGridModule', [])
 			}
 			return c;
 		}
-
 		function getHexSize(x) {
 			return (10 - Math.floor(x * 14)) + 40;
 		}
-        hexCanvas.recalculateAndDrawHexes = function(forceRedraw) {
-            hexCanvas.hexGrid = {
-                hexHeight: Math.sqrt(3) * getHexSize(hexCanvas.hexSize),
-                hexWidth: 2 * getHexSize(hexCanvas.hexSize),
-                hexSide: (3 / 2) * getHexSize(hexCanvas.hexSize),
+		function recalculateAndDrawHexes(forceRedraw) {
+			var size = getHexSize(hexSize);
+            hexGrid = {
+                hexHeight: Math.sqrt(3) * size,
+                hexWidth: 2 * size,
+                hexSide: (3 / 2) * size,
                 hexes:[]
             };
 
             var xPos,yPos;
             var offsetColumn = false;
-            var numCols = Math.floor((w - 255 - (2*hexScreenPadding)) / (hexCanvas.hexGrid.hexSide));
-            var numRows = Math.floor((h - (2*hexScreenPadding)) / (hexCanvas.hexGrid.hexHeight)) - 1;
+            var numCols = Math.floor((w - 255 - (2*hexScreenPadding)) / (hexGrid.hexSide));
+            var numRows = Math.floor((h - (2*hexScreenPadding)) / (hexGrid.hexHeight)) - 1;
 
             var counter = -1;
             for (var col = 0; col < numCols; col++) {
                 for (var row = 0; row < numRows; row++) {
                     counter++;
-                    xPos = col * hexCanvas.hexGrid.hexSide + hexScreenPadding;
-                    yPos = (row * hexCanvas.hexGrid.hexHeight) + (offsetColumn ? hexCanvas.hexGrid.hexHeight * 0.5 : 0) + hexScreenPadding;
+                    xPos = col * hexGrid.hexSide + hexScreenPadding;
+                    yPos = (row * hexGrid.hexHeight) + (offsetColumn ? hexGrid.hexHeight * 0.5 : 0) + hexScreenPadding;
                     var noteIndex = ((col%2 == 0 ? 0 : 9) + (Math.floor(col/2)+(row*5))) % notes.length;
 
-                    hexCanvas.hexGrid.hexes.push({
+                    hexGrid.hexes.push({
                         col: col,
                         row: row,
                         noteIndex: noteIndex,
                         index:counter,
                         frequency: Math.floor(440 * Math.pow(2, (noteIndex + (row * 12) - 48) / 12)),
                         sharp: noteIndex == 1 || noteIndex == 3 || noteIndex == 6 || noteIndex == 8 || noteIndex == 10,
-                        centerX: xPos - getHexSize(hexCanvas.hexSize) + hexCanvas.hexGrid.hexWidth,
-                        centerY: yPos + (hexCanvas.hexGrid.hexHeight / 2),
+                        centerX: xPos - getHexSize(hexSize) + hexGrid.hexWidth,
+                        centerY: yPos + (hexGrid.hexHeight / 2),
                         xArray:[
-                            xPos + hexCanvas.hexGrid.hexWidth - hexCanvas.hexGrid.hexSide,
-                            xPos + hexCanvas.hexGrid.hexSide,
-                            xPos + hexCanvas.hexGrid.hexWidth,
-                            xPos + hexCanvas.hexGrid.hexSide,
-                            xPos + hexCanvas.hexGrid.hexWidth - hexCanvas.hexGrid.hexSide,
+                            xPos + hexGrid.hexWidth - hexGrid.hexSide,
+                            xPos + hexGrid.hexSide,
+                            xPos + hexGrid.hexWidth,
+                            xPos + hexGrid.hexSide,
+                            xPos + hexGrid.hexWidth - hexGrid.hexSide,
                             xPos
                         ],
                         yArray:[
                             yPos,
                             yPos,
-                            yPos + (hexCanvas.hexGrid.hexHeight / 2),
-                            yPos + hexCanvas.hexGrid.hexHeight,
-                            yPos + hexCanvas.hexGrid.hexHeight,
-                            yPos + (hexCanvas.hexGrid.hexHeight / 2)
+                            yPos + (hexGrid.hexHeight / 2),
+                            yPos + hexGrid.hexHeight,
+                            yPos + hexGrid.hexHeight,
+                            yPos + (hexGrid.hexHeight / 2)
                         ]
                     });
                 }
                 offsetColumn = !offsetColumn;
             }
-            hexCanvas.checkHexes(forceRedraw);
-        };
+            checkHexes(forceRedraw);
+        }
 
-        hexCanvas.drawHex = function( hex, hover , surround ) {
+        function drawHex(hex, hover) {
             // actual hex
             ctx.beginPath();
             ctx.lineWidth = hover ? 2 : 1;
@@ -125,55 +144,52 @@ angular.module('hexGridModule', [])
             ctx.fillStyle = hover ? themeService.themes[themeService.themeIndex].hexActive.text : themeService.themes[themeService.themeIndex].hex.text;
             ctx.textAlign = 'center';
 
-            ctx.font="bold  12px Georgia";ctx.fillText(notes[hex.noteIndex], hex.centerX, hex.centerY-10);
-            ctx.font="10px Georgia";ctx.fillText(
+            ctx.font='bold  12px Georgia';
+	        ctx.fillText(notes[hex.noteIndex], hex.centerX, hex.centerY-10);
+            ctx.font='10px Georgia';ctx.fillText(
                 hex.frequency
-                //hex.col +"-"+ hex.row +"-"+hex.index
+                //hex.col +'-'+ hex.row +'-'+hex.index
                 , hex.centerX, hex.centerY+18);
             ctx.closePath();
 
-        };
+        }
 
-        hexCanvas.checkHexes = function(overRide) {
+		function checkHexes(overRide) {
             if (controlsService.controlsIndex == 0) {
                 //get hover index
                 if (!controlsService.events.mouseDown) {
-                    hexCanvas.hoverIndex = -1;
-                    for (var i = 0; i < hexCanvas.hexGrid.hexes.length; i++){
-                        if (pointInPolygon(hexCanvas.hexGrid.hexes[i].xArray,hexCanvas.hexGrid.hexes[i].yArray,controlsService.events.mouseX,controlsService.events.mouseY)) {
-                            hexCanvas.hoverIndex = i;
+                    hoverIndex = -1;
+                    for (var i = 0; i < hexGrid.hexes.length; i++){
+                        if (pointInPolygon(hexGrid.hexes[i].xArray,hexGrid.hexes[i].yArray,controlsService.events.mouseX,controlsService.events.mouseY)) {
+                            hoverIndex = i;
                             break;
                         }
                     }
                 }
             }
             if (controlsService.controlsIndex == 1 && overRide) {
-                hexCanvas.hoverIndex = -1;
-                for (i = 0; i < hexCanvas.hexGrid.hexes.length; i++){
-                    if (pointInPolygon(hexCanvas.hexGrid.hexes[i].xArray,hexCanvas.hexGrid.hexes[i].yArray,controlsService.events.mouseX,controlsService.events.mouseY)) {
-                        hexCanvas.hoverIndex = i;
+                hoverIndex = -1;
+                for (i = 0; i < hexGrid.hexes.length; i++){
+                    if (pointInPolygon(hexGrid.hexes[i].xArray,hexGrid.hexes[i].yArray,controlsService.events.mouseX,controlsService.events.mouseY)) {
+                        hoverIndex = i;
                         break;
                     }
                 }
             }
 
             //-----------------------------------------------------draw hexes
-            if (hexCanvas.hoverIndex != lastHoverIndex || overRide) {
+            if (hoverIndex != lastHoverIndex || overRide) {
                 ctx.clearRect(0, 0, w, h);
-                for (i = 0; i < hexCanvas.hexGrid.hexes.length; i++){
-                    if (i != hexCanvas.hoverIndex) { hexCanvas.drawHex(hexCanvas.hexGrid.hexes[i], false); }
+                for (i = 0; i < hexGrid.hexes.length; i++){
+                    if (i != hoverIndex) { drawHex(hexGrid.hexes[i], false); }
                 }
             }
             //draw hover hex last, so that the border stands out
-            if ((hexCanvas.hoverIndex != -1)) {
-                hexCanvas.drawHex(hexCanvas.hexGrid.hexes[hexCanvas.hoverIndex],true);
+            if ((hoverIndex != -1)) {
+                drawHex(hexGrid.hexes[hoverIndex],true);
             }
             //play notes
-            hexCanvas.hoverIndex == -1 ? audioService.stopHexSound() : audioService.playHexSound(hexCanvas.hexGrid.hexes[hexCanvas.hoverIndex].frequency);
-            lastHoverIndex = hexCanvas.hoverIndex;
+            hoverIndex == -1 ? audioService.stopHexSound() : audioService.playHexSound(hexGrid.hexes[hoverIndex].frequency);
+            lastHoverIndex = hoverIndex;
         };
-
-        hexCanvas.windowResize();
-        hexCanvas.recalculateAndDrawHexes(true);
-
     }
